4ba5c9d4d7997fab22fca2b334b185d050e97c13
hermasyp <hermas.yuda@gmail.com>
2019-12-02 17:46:15 +0700

    adding epg item view

---
+++ app/src/main/java/com/catnip/moviegate/datasource/detailmovies/DetailMovieDataSource.kt
@@ -0,0 +1,8 @@
+package com.catnip.moviegate.datasource.detailmovies
+
+/**
+Written with love by Muhammad Hermas Yuda Pamungkas
+Github : https://github.com/hermasyp
+ **/
+class DetailMovieDataSource {
+}
