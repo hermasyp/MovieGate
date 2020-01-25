3beacb0f3aeeef338d52f60e93b484b8fe72c919
hermasyp <hermas.yuda@gmail.com>
2020-01-25 23:28:44 +0700

    starting scheduler notification

---
+++ app/src/main/java/com/catnip/moviegate/services/UserNotifyManager.kt
@@ -0,0 +1,17 @@
+package com.catnip.moviegate.services
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+/**
+Written with love by Muhammad Hermas Yuda Pamungkas
+Github : https://github.com/hermasyp
+ **/
+
+class UserNotifyManager : BroadcastReceiver(){
+    override fun onReceive(context: Context?, intent: Intent?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
